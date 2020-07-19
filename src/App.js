c7e7cc88afba2855b38c26e3da4b399880547273
do <ngovando261297@gmail.com>
2020-07-19 12:00:21 +0700

    pagination-demo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,96 @@
-import React  from 'react';
-import './App.css'
-const App =()=> {
+import React, { useState, useEffect } from "react";
+import logo from "./download.png";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { data } from "./services/data";
+const App = () => {
+  const [pageIndex, setPageIndex] = useState(1);
+  const [rows, setRows] = useState([]);
+  const itemPerPages = 5; // so phan tu 1 trang
+  const pageOfTable = Math.ceil(data.length / itemPerPages);
+
+  useEffect(() => {
+    setRows(renderRow());
+  }, [pageIndex]);
+
+  const handleNext = () => {
+    if (pageIndex < pageOfTable) {
+      setPageIndex(pageIndex + 1);
+    }
+    return;
+  };
+
+  const handlePre = () => {
+    if (pageIndex > 1) {
+      setPageIndex(pageIndex - 1);
+    }
+    return;
+  };
+
+  const renderRow = () => {
+    const start = (pageIndex - 1) * itemPerPages;
+    const end = itemPerPages * pageIndex; //
+    const newArray = data.slice(start, end);
+    return newArray;
+  };
+  const renderTable = () => {
+    renderRow();
+    return (
+      <table class="table">
+        <thead class="thead-light">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map((row, index) => {
+            return (
+              <tr>
+                <th scope="row">
+                  {(pageIndex - 1) * itemPerPages + index + 1}
+                </th>
+                <td>{row.n}</td>
+                <td>{row.l}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+  };
+
+  const renderPagi = () => {
+    let result = [];
+    for (let i = 0; i < pageOfTable; i++) {
+      result.push(
+        <li class={`pageNumber ${i + 1 === pageIndex ? "active" : ""}`}>
+          <a href="#">{i + 1}</a>
+        </li>
+      );
+    }
+    return result;
+  };
   return (
-        <div class="form">
-          <input type="text" name="name" autocomplete="off" required />
-          <label for="name" class="label-name-input">
-            <span class="content-label-name">Name</span>
-          </label>
-        </div>
+    <>
+      {renderTable()}
+      <ul class="pagination">
+        <li>
+          <div onClick={handlePre} class="prev">
+            Pre
+          </div>
+        </li>
+        {renderPagi().map((item) => {
+          return item;
+        })}
+        <li>
+          <div onClick={handleNext} class="next">
+            Next{" "}
+          </div>
+        </li>
+      </ul>
+    </>
   );
-}
+};
 export default App;
---
+++ src/services/data.js
@@ -0,0 +1,63 @@
+export const data = [
+  {
+    n: "item 1",
+    l: "l-item1",
+  },
+  {
+    n: "item 2",
+    l: "l-item2",
+  },
+  {
+    n: "item 3",
+    l: "l-item3",
+  },
+  {
+    n: "item 4",
+    l: "l-item4",
+  },
+  {
+    n: "item 5",
+    l: "l-item5",
+  },
+  {
+    n: "item 6",
+    l: "l-item6",
+  },
+  {
+    n: "item 7",
+    l: "l-item7",
+  },
+  {
+    n: "item 8",
+    l: "l-item8",
+  },
+  {
+    n: "item 9",
+    l: "l-item9",
+  },
+
+  {
+    n: "item 10",
+    l: "l-item10",
+  },
+  {
+    n: "item 11",
+    l: "l-item11",
+  },
+  {
+    n: "item 12",
+    l: "l-item12",
+  },
+  {
+    n: "item 13",
+    l: "l-item13",
+  },
+  {
+    n: "item 14",
+    l: "l-item14",
+  },
+  {
+    n: "item 15",
+    l: "l-item15",
+  },
+];
